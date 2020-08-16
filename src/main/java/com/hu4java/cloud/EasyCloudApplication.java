74d7d6a0451b81393ee2d026eebcb604c15bd457
hu4java <chenzhenhu5@163.com>
2020-08-16 10:07:31 +0800

    init...

---
+++ src/main/java/com/hu4java/cloud/EasyCloudApplication.java
@@ -0,0 +1,13 @@
+package com.hu4java.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hu4java/cloud/EasyCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu4java.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
